bfa30a6f393b00c60647a716d2d2c96bcb5cc7fb
Meldrick Carabeo <meldrick.carabeo@gmail.com>
2024-05-13 21:11:04 +0800

    Update Book.java

--- Book.java
+++ Book.java
@@ -1,25 +1,7 @@
 public class Book implements LibraryResource {
-    private String title;
-
-    public Book(String title) {
-        this.title = title;
-    }
-
     @Override
-    public void borrow(Student student) {
+    public void borrow(Student student, String title) {
         // Implement borrowing a book
-    }
-}
-
-public class Journal implements LibraryResource {
-    private String title;
-
-    public Journal(String title) {
-        this.title = title;
-    }
-
-    @Override
-    public void borrow(Student student) {
-        // Implement borrowing a journal
+        System.out.println("Borrowed book: " + title);
     }
 }
