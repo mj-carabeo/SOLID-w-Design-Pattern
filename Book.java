23cc13e301c309e785efedcc36973cc8dae46ae1
Meldrick Carabeo <meldrick.carabeo@gmail.com>
2024-05-13 21:11:39 +0800

    Update Book.java

--- Book.java
+++ Book.java
@@ -1,7 +1,6 @@
 public class Book implements LibraryResource {
     @Override
     public void borrow(Student student, String title) {
-        // Implement borrowing a book
         System.out.println("Borrowed book: " + title);
     }
 }
